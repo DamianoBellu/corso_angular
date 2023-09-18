bc80a2d3aaf2beb277896e4d9f791c5943558bb9
Damiano Bellu <belludamiano9@gmail.com>
2023-09-18 10:51:21 +0200

    gestione prodotti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalciatoriComponent } from './calciatori/calciatori.component';
+import { ProdottiComponent } from './prodotti/prodotti.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalciatoriComponent,
+    ProdottiComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/calciatori/calciatori.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calciatori',
+  templateUrl: './calciatori.component.html',
+  styleUrls: ['./calciatori.component.css']
+})
+export class CalciatoriComponent {
+
+}
