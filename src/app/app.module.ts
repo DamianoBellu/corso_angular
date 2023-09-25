2061053768559e1eac3fff4ee4e569562b5e1e09
Damiano Bellu <belludamiano9@gmail.com>
2023-09-25 18:12:58 +0200

    To Do List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,12 @@ import { CalciatoriComponent } from './calciatori/calciatori.component';
 import { PiedeComponent } from './piede/piede.component';
 import { CalciatorePreviewComponent } from './calciatore-preview/calciatore-preview.component';
 import { CalciatoreDetailComponent } from './calciatore-detail/calciatore-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {BsDatepickerModule} from "ngx-bootstrap/datepicker";
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +20,16 @@ import { CalciatoreDetailComponent } from './calciatore-detail/calciatore-detail
     CalciatoriComponent,
     PiedeComponent,
     CalciatorePreviewComponent,
-    CalciatoreDetailComponent
+    CalciatoreDetailComponent,
+    TodoListComponent,
+    TodoItemComponent,
+    TodoAddComponent
   ],
   imports: [
+    BsDatepickerModule.forRoot(),
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data/todos.ts
@@ -0,0 +1,5 @@
+export const TODOS = [
+    {id:1, text: "fare la spesa", completed: false},
+    {id:2, text: "tagliare il prato", completed: false},
+    {id:3, text: "suonare la chitarra", completed: false}
+]
